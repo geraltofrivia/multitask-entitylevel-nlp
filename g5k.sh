72560510a237772ec5dba8ae1c3d00a5fd66d821
Priyansh Trivedi <priyansh.trivedi@inria.fr>
2022-03-29 19:16:15 +0200

    Time to let the model train (FINALLY)

--- g5k.sh
+++ g5k.sh
@@ -2,4 +2,4 @@
 # File used to run experiments on G5k
 # #############
 
-oarsub -l gpu=1,walltime=8 "source ~/.bashrc; conda activate main;cd ~/work/coref;python src/run.py -d ontonotes -t ner -enc bert-base-uncased -e 5 -dv cuda"
+oarsub -l gpu=1,walltime=8 "source ~/.bashrc; conda activate main;cd ~/work/coref;python src/run.py -d ontonotes -t ner -enc bert-base-uncased -e 200 -dv cuda"
