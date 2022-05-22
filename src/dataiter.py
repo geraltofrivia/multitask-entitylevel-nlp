9bfc11f28d2b0bdae7d5fa02a0ac7596d65a8762
Priyansh Trivedi <priyansh.trivedi@inria.fr>
2022-05-22 14:54:28 +0200

    no trim should be 50, not 7

--- src/dataiter.py
+++ src/dataiter.py
@@ -133,7 +133,7 @@ class MultiTaskDataIter(Dataset):
 
         if self.config.trim:
             warnings.warn("The dataset has been trimmed to only 50 instances. This is NOT a legit experiment any more!")
-            self.data = self.data[:7]
+            self.data = self.data[:50]
 
     def estimate_class_weights(self, task: str) -> List[float]:
         """
