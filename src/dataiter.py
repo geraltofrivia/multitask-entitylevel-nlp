1. NER dataiter stuff is resolved. For regular datasets, the NER computation was just plain wrong. Fixed that. 2. Eval metrics have been reworked. Previously the pruner and NER spanrec while being binary tasks were being treated as multiclass tasks which makes little sense. Fixed. Henceforth, no macro metrics. (also removed them from run.py) 3. Slightly less interruptions when preproc is going on. 4. Model code reworked to actually correctly process single label NER tasks. This part is still kinda untested.

src/dataiter.py
@@ -489,7 +489,7 @@ class MultiTaskDataIter(Dataset):
         if self._src_ in NER_IS_MULTILABEL:
             gold_labels = torch.tensor(gold_labels, dtype=torch.long, device='cpu')
         else:
-            gold_labels = torch.tensor([max(doc_ner) for doc_ner in gold_labels],
+            gold_labels = torch.tensor([np.argmax(doc_ner) for doc_ner in gold_labels],
                                        dtype=torch.long, device='cpu')
 
         ner_specific = {

src/models/multitask.py
@@ -18,7 +18,7 @@ try:
 except ImportError:
     from . import _pathfix
 from utils.data import Tasks
-from config import _SEED_ as SEED
+from config import _SEED_ as SEED, NER_IS_MULTILABEL
 from preproc.encode import Retriever
 from models.modules import SpanPruner, CorefDecoder, SharedDense
 from utils.exceptions import AnticipateOutOfMemException, UnknownDomainException, NANsFound
@@ -163,6 +163,7 @@ class MangoesMTL(nn.Module):
         self.max_training_segments = max_training_segments
         self.coref_depth = coref_higher_order
         self.coref_loss_mean = coref_loss_mean
+        self.ner_n_classes = {task.dataset: task.n_classes_ner for task in [task_1, task_2]}
         self._skip_instance_after_nspan = skip_instance_after_nspan
         self._ignore_speaker = ignore_speakers
         self._freeze_encoder = freeze_encoder
@@ -226,7 +227,7 @@ class MangoesMTL(nn.Module):
         same_end = torch.eq(labeled_ends.unsqueeze(1), candidate_ends.unsqueeze(0))  # [num_labeled, num_candidates]
         same_span = torch.logical_and(same_start, same_end)  # [num_labeled, num_candidates]
         # type casting in next line is due to torch not supporting matrix multiplication for Long tensors
-        if labels.shape.__len__ == 1:
+        if labels.shape.__len__() == 1:
             candidate_labels = torch.mm(labels.unsqueeze(0).float(), same_span.float()).long()  # [1, num_candidates]
         else:
             candidate_labels = torch.mm(same_span.transpose(1, 0).float(), labels.float())  # [nclasses, num_candidates]
@@ -609,9 +610,14 @@ class MangoesMTL(nn.Module):
             """
                 At this point NER Labels is a n_spans, n_classes+1 matrix where most rows are zero.
                 We want to turn all those zero rows into ones where the last element is active (not an entity class)
+                If NER is not a multilabel class, we want to turn those indices which have a zero to n_classes
             """
-            zero_indices = torch.sum(ner_labels, dim=1) == 0  # n_spans
-            ner_labels[zero_indices, -1] = 1
+            if domain in NER_IS_MULTILABEL:
+                zero_indices = torch.sum(ner_labels, dim=1) == 0  # n_spans
+                ner_labels[zero_indices, -1] = 1  # n_spans, n_classes+1
+            else:
+                zero_indices = ner_labels == 0
+                ner_labels[zero_indices] = self.ner_n_classes[domain]
 
             # Calculating the loss
             # if self.ner_unweighted:

src/run.py
@@ -24,8 +24,8 @@ from dataiter import MultiTaskDataIter, MultiDomainDataCombiner
 from utils.misc import check_dumped_config, merge_configs, SerializedBertConfig
 from config import LOCATIONS as LOC, DEFAULTS, KNOWN_SPLITS, _SEED_ as SEED, SCHEDULER_CONFIG, NER_IS_MULTILABEL
 from utils.exceptions import ImproperDumpDir, BadParameters
-from eval import Evaluator, NERAcc, NERSpanRecognitionMicro, NERSpanRecognitionMacro, \
-    PrunerPRMicro, PrunerPRMacro, CorefBCubed, CorefMUC, CorefCeafe, TraceCandidates, NERMultiLabelAcc
+from eval import Evaluator, NERAcc, NERSpanRecognitionMicro, PrunerPRMicro, CorefBCubed, CorefMUC, CorefCeafe, \
+    TraceCandidates, NERMultiLabelAcc
 
 random.seed(SEED)
 np.random.seed(SEED)
@@ -499,15 +499,17 @@ def train(ctx):
         metrics[task.dataset] += [TraceCandidates]
 
         if 'ner' in task:
-            metrics[task.dataset] += [NERAcc if not task.dataset in NER_IS_MULTILABEL else \
-                                          partial(NERMultiLabelAcc, nc=task.n_classes_ner,
-                                                  threshold=config.ner_threshold),
-                                      partial(NERSpanRecognitionMicro, device=config.device),
-                                      partial(NERSpanRecognitionMacro, n_classes=task.n_classes_ner,
-                                              device=config.device)]
+            metrics[task.dataset] += [
+                NERAcc if not task.dataset in NER_IS_MULTILABEL else
+                partial(NERMultiLabelAcc, nc=task.n_classes_ner, threshold=config.ner_threshold),
+                partial(NERSpanRecognitionMicro, device=config.device),
+                # partial(NERSpanRecognitionMacro, n_classes=task.n_classes_ner, device=config.device)
+            ]
         if 'pruner' in task:
-            metrics[task.dataset] += [partial(PrunerPRMicro, device=config.device),
-                                      partial(PrunerPRMacro, n_classes=task.n_classes_pruner, device=config.device)]
+            metrics[task.dataset] += [
+                partial(PrunerPRMicro, device=config.device),
+                # partial(PrunerPRMacro, n_classes=task.n_classes_pruner, device=config.device)
+            ]
         if 'coref' in task:
             metrics[task.dataset] += [CorefBCubed, CorefMUC, CorefCeafe]
 