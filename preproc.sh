4fdb49ca1b88eef9337c603b1eba8e926c6d9bd1
Priyansh Trivedi <priyansh.trivedi@inria.fr>
2022-08-20 11:05:53 +0200

    added dwie to preproc as well

--- preproc.sh
+++ preproc.sh
@@ -4,6 +4,7 @@ rm -r data/encoded/*
 python src/preproc/ontonotes.py -a
 python src/preproc/scierc.py -s all
 python src/preproc/codicrac.py -a
+python src/preproc/dwie.py
 
 # For my next trick, if you have ze GPUs you wanna run (For all datasets).
 # Replace the -enc with whatever you're currently using
